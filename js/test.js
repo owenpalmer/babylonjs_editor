d8af1e396a8936b65c0d122d34b079b77460b875
Owen Palmer <speedcubegeek@gmail.com>
2022-04-08 18:53:26 -0700

    Use actions for selecting. Highlight selected objects. Grid still interferes with selections, must be fixed

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("renderCanvas"); // Get the canvas element
-const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+const engine = new BABYLON.Engine(canvas, true, { stencil: true }); // Generate the BABYLON 3D engine
 
 function makeScene() {
     const scene = new BABYLON.Scene(engine);
@@ -13,13 +13,29 @@ function makeScene() {
     camera.inputs.attached.pointers.buttons[0] = null;
 
     gizmoman = new BABYLON.GizmoManager(scene);
+    gizmoman.usePointerToAttachGizmos = false;
     gizmoman.positionGizmoEnabled = true;
     gizmoman.attachableMeshes = [];
     gizmoman.onAttachedToMeshObservable.add((mesh) => {
         console.log(mesh.name);
     });
 
-    createViewportGrid(scene);
+    ground = createViewportGrid(scene);
+
+    highlight = new BABYLON.HighlightLayer("hl1", scene);
+    highlight.addExcludedMesh(ground);
+    highlight_blur = .4;
+    highlight.blurHorizontalSize = highlight_blur;
+    highlight.blurVerticalSize = highlight_blur;
+    highlight.innerGlow = false;
+    highlight.disableBoundingBoxesFromEffectLayer = false;
+
+    actionManager = new BABYLON.ActionManager(scene);
+    actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function(ev){
+        highlight.removeAllMeshes();
+        highlight.addMesh(ev.meshUnderPointer, BABYLON.Color3.Yellow());
+        gizmoman.attachToMesh(ev.meshUnderPointer);
+    }));
 
     const light = new BABYLON.DirectionalLight("thelight", new BABYLON.Vector3(-1,-1,0), scene);
 
@@ -80,18 +96,27 @@ function createSphere() {
     const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {}, scene);
     gizmoman.attachableMeshes.push(sphere);
     gizmoman.attachToMesh(sphere);
+    sphere.actionManager = actionManager;
+    highlight.removeAllMeshes();
+    highlight.addMesh(sphere, BABYLON.Color3.Yellow());
 }
 
 function createCube() {
     const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
     gizmoman.attachableMeshes.push(box);
     gizmoman.attachToMesh(box);
+    box.actionManager = actionManager;
+    highlight.removeAllMeshes();
+    highlight.addMesh(box, BABYLON.Color3.Yellow());
 }
 
 function createCylinder() {
     const cylinder = BABYLON.MeshBuilder.CreateCylinder("cylinder", {}, scene);
     gizmoman.attachableMeshes.push(cylinder);
     gizmoman.attachToMesh(cylinder);
+    cylinder.actionManager = actionManager;
+    highlight.removeAllMeshes();
+    highlight.addMesh(cylinder, BABYLON.Color3.Yellow());
 }
 
 function createPointLight() {
@@ -102,6 +127,9 @@ function createPointLight() {
     light.parent = light_dummy;
     gizmoman.attachableMeshes.push(light_dummy);
     gizmoman.attachToMesh(light_dummy);
+    light_dummy.actionManager = actionManager;
+    highlight.removeAllMeshes();
+    highlight.addMesh(light_dummy, BABYLON.Color3.Yellow());
 }
 
 function createViewportGrid(scene) {
@@ -115,4 +143,5 @@ function createViewportGrid(scene) {
 	groundMaterial.opacity = 0.2;
 	var ground = BABYLON.Mesh.CreateGround("ground1", 100, 100, 2, scene);
 	ground.material = groundMaterial;
+    return ground;
 }
